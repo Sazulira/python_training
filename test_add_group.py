d364432b1bed13bc2abdfe32ab52c55f1fe4383d
Alisa Razumova <razumova.alisa@gmail.com>
2019-05-04 18:22:09 +0300

    Внести изменения в метод Create group

--- test_add_group.py
+++ test_add_group.py
@@ -13,7 +13,16 @@ class TestAddGroup(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.create_group(wd)
+        self.create_group(wd, name="group1", header="new group", footer="new group040519")
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.create_group(wd, name="", header="", footer="")
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -23,17 +32,17 @@ class TestAddGroup(unittest.TestCase):
     def return_to_groups_page(self, wd):
         # return to groups page
         wd.find_element_by_link_text("group page").click()
-    def create_group(self, wd):
+    def create_group(self, wd, name, header, footer):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("group1")
+        wd.find_element_by_name("group_name").send_keys(name)
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("new group")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("new group040519")
+        wd.find_element_by_name("group_footer").send_keys(footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
     def login(self, wd, username, password):
