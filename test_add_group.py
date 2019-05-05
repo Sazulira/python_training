c960db6beca9a9a7f94b8cf9ae8df58383f21c2f
Alisa Razumova <razumova.alisa@gmail.com>
2019-05-05 12:32:15 +0300

    Передали ссылку на драйвер в вспомогательные методы

--- test_add_group.py
+++ test_add_group.py
@@ -11,11 +11,14 @@ class TestAddGroup(unittest.TestCase):
         self.wd.implicitly_wait(30)
 
 
-    def logout(self, wd):
+    def logout(self):
+        wd = self.wd
         wd.find_element_by_link_text("Logout").click()
-    def return_to_groups_page(self, wd):
+    def return_to_groups_page(self):
+        wd = self.wd
         wd.find_element_by_link_text("group page").click()
-    def create_group(self, wd, group):
+    def create_group(self, group):
+        wd = self.wd
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -28,32 +31,32 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
-        self.return_to_groups_page(wd)
-    def login(self, wd, username, password):
-        self.open_home_page(wd)
+        self.return_to_groups_page()
+    def login(self, username, password):
+        wd = self.wd
+        self.open_home_page()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
-    def open_home_page(self, wd):
+    def open_home_page(self):
+        wd = self.wd
         wd.get("http://localhost/addressbook/group.php")
 
 
 
     def test_add_group(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.create_group(wd, Group(name="group1", header="new group", footer="new group040519"))
-        self.logout(wd)
+        self.login(username="admin", password="secret")
+        self.create_group(Group(name="group1", header="new group", footer="new group040519"))
+        self.logout()
 
 
     def test_add_empty_group(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.create_group(wd, Group(name="", header="", footer=""))
-        self.logout(wd)
+        self.login(username="admin", password="secret")
+        self.create_group(Group(name="", header="", footer=""))
+        self.logout()
 
 
     def is_element_present(self, how, what):
