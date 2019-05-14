01d375485f5e59a261cbfcd71423cdfc0b2bcc75
Alisa Razumova <razumova.alisa@gmail.com>
2019-05-14 16:00:52 +0300

    черновик - дополнительны файл

---
+++ draft.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+# working ver
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest
+from data import Name
+from data import Tel
+from data import Web
+from data import Dates
+from data import Extra
+
+
+class TestAddContact(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+
+    def test_add_contact(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_add_new_contact_page(wd)
+        self.fill_form_name(wd,
+                            Name(firstname="Misha", middlename="MIO", lastname="Ivanov", nickname="MIOBN", title="MR",
+                                 company="Comp", address="Moscow"))
+        self.fill_form_telephone(wd, Tel(home="8495", mobile="8911", work="8812", fax="84951"))
+        self.fill_form_email(wd, Web(email="er", email2="qw", email3="yu", homepage="rewti"))
+        self.fill_form_bday(wd, Dates(bday="14", bmonth="November", byear="1986", aday="18", amonth="November",
+                                      ayear="1996"))
+        self.fill_form_secondary(wd, Extra(address2="SPb", phone2="Nevskiy", notes="none"))
+        self.submit_creation_new_contact(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def submit_creation_new_contact(self, wd):
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
+    def fill_form_secondary(self, wd, extra):
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(extra.address2)
+        wd.find_element_by_name("phone2").clear()
+        wd.find_element_by_name("phone2").send_keys(extra.phone2)
+        wd.find_element_by_name("notes").clear()
+        wd.find_element_by_name("notes").send_keys(extra.notes)
+
+    def fill_form_bday(self, wd, dates):
+        wd.find_element_by_name("bday").click()
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(dates.bday)
+        wd.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Birthday:'])[1]/following::option[16]").click()
+        wd.find_element_by_name("bmonth").click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(dates.bmonth)
+        wd.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Birthday:'])[1]/following::option[45]").click()
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(dates.byear)
+        wd.find_element_by_name("aday").click()
+        Select(wd.find_element_by_name("aday")).select_by_visible_text(dates.aday)
+        wd.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Anniversary:'])[1]/following::option[20]").click()
+        wd.find_element_by_name("amonth").click()
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text(dates.amonth)
+        wd.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Anniversary:'])[1]/following::option[45]").click()
+        wd.find_element_by_name("ayear").click()
+        wd.find_element_by_name("ayear").clear()
+        wd.find_element_by_name("ayear").send_keys(dates.ayear)
+
+    def fill_form_email(self, wd, web):
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(web.email)
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(web.email2)
+        wd.find_element_by_name("email3").clear()
+        wd.find_element_by_name("email3").send_keys(web.email3)
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(web.homepage)
+
+    def fill_form_telephone(self, wd, tel):
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(tel.home)
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(tel.mobile)
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(tel.work)
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(tel.fax)
+
+    def fill_form_name(self, wd, name):
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(name.firstname)
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(name.middlename)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(name.lastname)
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(name.nickname)
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(name.title)
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(name.company)
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(name.address)
+
+    def open_add_new_contact_page(self, wd):
+        wd.find_element_by_link_text("add new").click()
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
+    def is_element_present(self, how, what):
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+    def tearDown(self):
+        self.wd.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+# additional class
+class Name:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+
+class Tel:
+
+    def __init__(self, home, mobile, work, fax):
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+
+class Web:
+
+    def __init__(self, email, email2, email3, homepage):
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+
+class Dates:
+
+    def __init__(self, bday, bmonth, byear, aday, amonth, ayear):
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+
+class Extra:
+
+    def __init__(self, address2, phone2, notes):
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
+
+
+
+
