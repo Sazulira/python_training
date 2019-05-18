435bc1dfba9914f3d069bf545937322365b9d6f9
Alisa Razumova <razumova.alisa@gmail.com>
2019-05-18 19:18:07 +0300

    Выделен класс для работы с контактами

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,12 +11,12 @@ def app(request):
 
 def test_add_contact(app):
     app.open_home_page()
-    app.login(username="admin", password="secret")
-    app.open_add_new_contact_page()
-    app.fill_form_name( Contact(firstname="Misha", middlename="MIO", lastname="Ivanov", nickname="MIOBN", title="MR",
+    app.session.login(username="admin", password="secret")
+    app.contact.open_add_new_contact_page()
+    app.contact.fill_form_name(Contact(firstname="Misha", middlename="MIO", lastname="Ivanov", nickname="MIOBN", title="MR",
                                         company="Comp", address="Moscow", home="8495", mobile="8911", work="8812",
                                         fax="84951", email="er", email2="qw", email3="yu", homepage="rewti", bday="14",
                                         bmonth="November", byear="1986", aday="18", amonth="November", ayear="1996",
                                         address2="SPb", phone2="Nevskiy", notes="none"))
-    app.submit_creation_new_contact()
-    app.logout()
+    app.contact.submit_creation_new_contact()
+    app.session.logout()
