12d55663e7498a32769aa69d00a81aea25d328ab
Alisa Razumova <razumova.alisa@gmail.com>
2019-05-25 19:56:40 +0300

    в тест на изменение группы добавлена проверка наличия группы, в сл отсутсвия - создание новой группы, затем изменение

--- fixture/group.py
+++ fixture/group.py
@@ -53,3 +53,6 @@ class GroupHelper:
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
+    def count(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_name("selected[]"))
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,4 +1,7 @@
+from model.group import Group
 
 def test_delete_first_group(app):
     app.open_home_page_for_groups()
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.delete_first_group()
